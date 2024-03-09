6fef87b3821b3c03fdf26c7a67ec8b10d0b3dd27
system home <aaafewsh82@gmail.com>
2024-03-09 13:51:54 +0330

    test benyamin

---
+++ js/script.js
@@ -0,0 +1,3 @@
+var scroll_top = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+console.log(scroll_top);
