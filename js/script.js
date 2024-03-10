ed790050d793c40f625e7ef2b5e07384e94a4354
amir abbas <aaafewsh82@gmail.com>
2024-03-10 08:32:07 +0330

    a

--- js/script.js
+++ js/script.js
@@ -1,5 +1,55 @@
-var scroll_top = $(window).scrollTop();
+// var scroll_top = $(window).scrollTop();
 
-if (scroll_top > 100) {
-    $("#scrolled div").addClass("scrolled");
-}
+// if ($(window).scrollTop() > 100) {
+//     $("#scrolled").addClass("scrolled");
+// }
+
+
+// $("#scrolled").addClass("scrolled");
+
+
+// $(function () {Z
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() > 100) {
+//             $('#scroll-top').addClass('active-btn-down');
+//             $('#scroll-top').removeClass('d-none');
+//             $('#scroll-top').removeClass('active-btn-up');
+//         }
+//     });
+
+// $(document).ready(function() {
+//     var lastScrollTop = 0;
+//     var scrollRate = 0;
+  
+//     $(window).scroll(function(event) {
+//       var st = $(this).scrollTop();
+//       if (st > lastScrollTop){
+//           scrollRate = st - lastScrollTop;
+//       } else {
+//           scrollRate = lastScrollTop - st;
+//       }
+//       lastScrollTop = st;
+//       $("#scroll-indicator").text("Scroll Rate: " + scrollRate + "px");
+//     });
+//   });
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+      // Set the number you want to trigger the function at
+      var scrollNumber = 100;
+      
+      // Check if the scroll position has reached the specified number
+      if ($(this).scrollTop() >= scrollNumber) {
+        // Call your special function here
+        specialFunction();
+      } else {
+        $("#scrolled").removeClass("scrolled");
+      }
+    });
+  
+    // Define your special function
+    function specialFunction() {
+      // Do something special when the scroll position reaches the specified number
+      $("#scrolled").addClass("scrolled");
+    }
+  });
