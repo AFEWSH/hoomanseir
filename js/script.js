c677c14b66eef9608b2b5133bc849954b686285d
system home <aaafewsh82@gmail.com>
2024-04-18 11:05:15 +0330

    test benyamin

--- js/script.js
+++ js/script.js
@@ -1,37 +1,3 @@
-// var scroll_top = $(window).scrollTop();
-
-// if ($(window).scrollTop() > 100) {
-//     $("#scrolled").addClass("scrolled");
-// }
-
-
-// $("#scrolled").addClass("scrolled");
-
-
-// $(function () {Z
-//     $(window).scroll(function () {
-//         if ($(this).scrollTop() > 100) {
-//             $('#scroll-top').addClass('active-btn-down');
-//             $('#scroll-top').removeClass('d-none');
-//             $('#scroll-top').removeClass('active-btn-up');
-//         }
-//     });
-
-// $(document).ready(function() {
-//     var lastScrollTop = 0;
-//     var scrollRate = 0;
-  
-//     $(window).scroll(function(event) {
-//       var st = $(this).scrollTop();
-//       if (st > lastScrollTop){
-//           scrollRate = st - lastScrollTop;
-//       } else {
-//           scrollRate = lastScrollTop - st;
-//       }
-//       lastScrollTop = st;
-//       $("#scroll-indicator").text("Scroll Rate: " + scrollRate + "px");
-//     });
-//   });
 
 $(document).ready(function() {
     $(window).scroll(function() {
@@ -53,3 +19,162 @@ $(document).ready(function() {
       $("#scrolled").addClass("scrolled");
     }
   });
+
+  $(document).ready(function() {
+    $(".my_select").select2({
+      placeholder: "نام شهر مبدا",
+  });
+    $(".my_select1").select2({
+      placeholder: "نام شهر مقصد",
+  });
+    $(".my_select2").select2({
+      placeholder: "تاریخ مسافرت",
+      allowClear: true,
+  });
+    $(".my_select3").select2({
+      placeholder: "کشور مبدا : ایران",
+  });
+    $(".my_select4").select2({
+      placeholder: " شهر مبدا",
+  });
+    $(".my_select5").select2({
+      placeholder: "کشور مقصد",
+  });
+    $(".my_select6").select2({
+      placeholder: " شهر مقصد",
+  });
+    $(".my_select7").select2({
+      placeholder: "تاریخ مسافرت",
+      allowClear: true,
+  });
+    $(".my_select8").select2({
+      placeholder: "قاره",
+  });
+    $(".my_select9").select2({
+      placeholder: "مقصد",
+  });
+    $(".my_select10").select2({
+      placeholder: "نوع ویزا",
+  });
+    $(".my_select11").select2({
+      placeholder: "تاریخ مسافرت",
+      allowClear: true,
+  });
+
+	});
+
+  jQuery(document).ready(function($){
+    var offset = 100;
+    var speed = 250;
+    var duration = 500;
+	   $(window).scroll(function(){
+            if ($(this).scrollTop() < offset) {
+			     $('.topbutton') .fadeOut(duration);
+            } else {
+			     $('.topbutton') .fadeIn(duration);
+            }
+        });
+	$('.topbutton').on('click', function(){
+		$('html, body').animate({scrollTop:0}, speed);
+		return false;
+		});
+});
+
+
+
+$(document).ready(function() {
+
+var My_Width = screen.width;
+ 
+if( My_Width <= 576 ){
+  var owl = $('.owl-carousel');
+owl.owlCarousel({
+    rtl:true,
+    items:1,
+    loop:false,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+
+  $("#hotel-van-carousel").removeClass("d-none");
+  $("#hotel-van").addClass("d-none");
+} else if (My_Width <= 992) {
+  var owl = $('.owl-carousel');
+  owl.owlCarousel({
+      rtl:true,
+      items:2,
+      loop:false,
+      margin:10,
+      autoplay:true,
+      autoplayTimeout:3000,
+      autoplayHoverPause:true
+  });
+  $('.play').on('click',function(){
+      owl.trigger('play.owl.autoplay',[1000])
+  })
+  $('.stop').on('click',function(){
+      owl.trigger('stop.owl.autoplay')
+  })
+  
+    $("#hotel-van-carousel").removeClass("d-none");
+    $("#hotel-van").addClass("d-none");
+}
+
+});
+
+$(document).ready(function() {
+
+  var My_Width = screen.width;
+   
+  if( My_Width <= 576 ){
+    var owl = $('.owl-carousel');
+  owl.owlCarousel({
+      rtl:true,
+      items:1,
+      loop:false,
+      margin:10,
+      autoplay:true,
+      autoplayTimeout:3000,
+      autoplayHoverPause:true
+  });
+  $('.play').on('click',function(){
+      owl.trigger('play.owl.autoplay',[1000])
+  })
+  $('.stop').on('click',function(){
+      owl.trigger('stop.owl.autoplay')
+  })
+  
+    $("#tours-carousel").removeClass("d-none");
+    $("#tours").addClass("d-none");
+  } else if (My_Width <= 992) {
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        rtl:true,
+        items:2,
+        loop:false,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true
+    });
+    $('.play').on('click',function(){
+        owl.trigger('play.owl.autoplay',[1000])
+    })
+    $('.stop').on('click',function(){
+        owl.trigger('stop.owl.autoplay')
+    })
+    
+      $("#tours-carousel").removeClass("d-none");
+      $("#tours").addClass("d-none");
+  }
+  
+  });
+  
---
+++ test.js
@@ -0,0 +1,7 @@
+My_Width = screen.width;
+ 
+if( My_Width <= 700 ){
+ alert("اندازه کمتر از ۷۰۰ هست");
+}else{
+ alert("اندازه بیشتر از ۷۰۰ هست");
+}
